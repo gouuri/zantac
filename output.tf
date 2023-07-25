a3d2756cd8603f9082d551db8acf0a4d92b7c823
gouuri <gouuri.sivam@gmail.com>
2023-07-25 12:58:58 +0530

    Revert "Add: IAC for webserver" Commit "code reversed" This reverts commit 8e58f481aeed56d43ee8d6ab3a4ded95614a993a.

--- output.tf
+++
@@ -1,3 +0,0 @@
-output "vmss_public_ip_fqdn" {
-   value = module.public_ip.public_ip_vmss_fqdn
-}
--- vnet/output.tf
+++
@@ -1,7 +0,0 @@
-output "vnet_id" {
-  value = azurerm_virtual_network.main.id
-}
-
-output "subnet_id" {
-  value = azurerm_subnet.main.id
-}
